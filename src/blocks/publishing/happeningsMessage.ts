2a0bfcedd085815a84bb2963330840d03e1e3d43
Mahad Kalam <mahadkalam1@proton.me>
2025-01-20 22:17:29 +0000

    Slack blocks generator

---
+++ src/blocks/publishing/happeningsMessage.ts
@@ -0,0 +1,12 @@
+import type { Story } from "../../airtable";
+
+export default (intro: string, conclusion: string, stories: Story[]) => {
+    const storyMrkdwn = stories.map(story => `*${story.headline}*\n${story.shortDescription}`).join("\n\n");
+    // Is this really the best way to do this?
+    // I used to have some stuff with sections and `shortDescriptionRt` and whatnot, but the problem was that it
+    // was really annoying to actually *read* the newsletter, so now we just send it as one big mrkdwn message.
+    return {
+        text: `${intro}\n\n${storyMrkdwn}\n\n${conclusion}`,
+        type: "mrkdwn"
+    }
+}
--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -3,7 +3,8 @@ import { db, storiesTable } from "../airtable";
 import { render } from "@react-email/components";
 import type Slack from "@slack/bolt";
 import Email from "../emails/newsletterEmail";
-import publishModal from "../blocks/publish/publishModal";
+import publishModal from "../blocks/publishing/publishModal";
+import buildHappeningsMessage from "../blocks/publishing/happeningsMessage";
 import { env } from "../env";
 import Plunk from "@plunk/node";
 import logger from "../logger";
@@ -44,40 +45,49 @@ export default function (app: Slack.App) {
             filterByFormula: `{status} = "Approved"`
         });
 
-        const emailHtml = await render(Email({ intro: "Arch Newsletter", conclusion: "Thanks for reading!", stories: approvedStories }));
-        logger.debug({ requestedBy: body.user.id }, "Sending newsletter");
+        await client.chat.postMessage({
+            channel: env.HAPPENINGS_CHANNEL_ID,
+            ...buildHappeningsMessage(
+                "Welcome back to #happenings, the weekly newsletter which is now 10 weeks old! Enjoy this double-digit edition :).",
+                "P.S. Send me a DM if you have cool community-oriented stuff, I.e. active YSWS, which you want featured (Tenth edition :yay: - Written by @Felix Gao, revised by @radioblahaj",
+                approvedStories
+            )
+        });
 
-        const headers = {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${env.PLUNK_API_KEY}`
-        };
-        const campaignOptions = {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({
-                subject: "Arch Newsletter",
-                body: emailHtml,
-                recipients: ["mahadkalam1@proton.me"],
-                style: "HTML"
-            })
-        };
-        const campaign = await fetch('https://api.useplunk.com/v1/campaigns', campaignOptions)
-            .then(response => response.json())
-        const sendOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${env.PLUNK_API_KEY}` },
-            body: JSON.stringify({
-                id: campaign.id,
-                live: true,
-                delay: 1
-            })
-        };
+        // const emailHtml = await render(Email({ intro: "Arch Newsletter", conclusion: "Thanks for reading!", stories: approvedStories }));
+        // logger.debug({ requestedBy: body.user.id }, "Sending newsletter");
 
-        const response = await fetch('https://api.useplunk.com/v1/campaigns/send', sendOptions);
-        if (!response.ok) {
-            throw new Error(`Failed to send campaign: ${response.statusText}`);
-        }
+        // const headers = {
+        //     'Content-Type': 'application/json',
+        //     Authorization: `Bearer ${env.PLUNK_API_KEY}`
+        // };
+        // const campaignOptions = {
+        //     method: 'POST',
+        //     headers,
+        //     body: JSON.stringify({
+        //         subject: "Arch Newsletter",
+        //         body: emailHtml,
+        //         recipients: ["mahadkalam1@proton.me"],
+        //         style: "HTML"
+        //     })
+        // };
+        // const campaign = await fetch('https://api.useplunk.com/v1/campaigns', campaignOptions)
+        //     .then(response => response.json())
+        // const sendOptions = {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${env.PLUNK_API_KEY}` },
+        //     body: JSON.stringify({
+        //         id: campaign.id,
+        //         live: true,
+        //         delay: 1
+        //     })
+        // };
 
-        logger.debug({ requestedBy: body.user.id }, "Sent newsletter");
+        // const response = await fetch('https://api.useplunk.com/v1/campaigns/send', sendOptions);
+        // if (!response.ok) {
+        //     throw new Error(`Failed to send campaign: ${response.statusText}`);
+        // }
+
+        // logger.debug({ requestedBy: body.user.id }, "Sent newsletter");
     })
 }
